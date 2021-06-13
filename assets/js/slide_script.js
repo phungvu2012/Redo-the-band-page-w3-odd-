1a8f6f3690a985ffdbed7f80e3d4380c1715922d
phungvu2012 <phungvuminh2012@gmail.com>
2021-06-13 16:36:28 +0700

    the band page (not javascript)

---
+++ assets/js/slide_script.js
@@ -0,0 +1,10 @@
+var sliderIndex = 1;
+function plusSlides(n) {
+
+}
+
+function showSlides(n) {
+    var slides = document.getElementById("#slider");
+    if()
+}
+document.getElementById("#slider").style.background = 
